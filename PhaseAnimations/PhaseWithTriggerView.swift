6a4dee5c30423b3db6597b140de13295626de781
Ramill Ibragimov <146063206+ribragimov-mwb@users.noreply.github.com>
2023-09-30 15:52:23 +0300

    Phase Animations in iOS 17

--- PhaseAnimations/PhaseWithTriggerView.swift
+++ PhaseAnimations/PhaseWithTriggerView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+enum PhaseState: Int, CaseIterable {
+    case first, second, third, fourth
+    
+    var color: Color {
+        switch self {
+            case .first: .yellow
+            case .second: .orange
+            default: .red
+        }
+    }
+    
+    var rotation: Double {
+        -1 * Double(self.rawValue * 90)
+    }
+    
+    var scale: Double {
+        Double(self.rawValue + 1)
+    }
+}
+
 struct PhaseWithTriggerView: View {
-    @State private var go = false
+    @State private var start = false
     
     var body: some View {
         NavigationStack {
             Image(systemName: "star")
                 .font(.system(size: 40))
-                .foregroundColor(.yellow)
+                .phaseAnimator(PhaseState.allCases, trigger: start) { content, phase in
+                    content
+                        .foregroundStyle(phase.color)
+                        .scaleEffect(phase.scale)
+                        .rotationEffect(.degrees(phase.rotation))
+                } animation: { _ in
+                        .bouncy(duration: 1.0)
+                }
                 .navigationTitle("Phase Trigger")
+                .onTapGesture {
+                    start.toggle()
+                }
         }
     }
 }
