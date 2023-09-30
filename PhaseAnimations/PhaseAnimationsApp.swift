05919a46545bb6925350d41add4bc82886d66a92
Ramill Ibragimov <146063206+ribragimov-mwb@users.noreply.github.com>
2023-09-30 14:50:26 +0300

    Initial Commit

---
+++ PhaseAnimations/PhaseAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhaseAnimationsApp.swift
+//  PhaseAnimations
+//
+//  Created by Ramill Ibragimov on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct PhaseAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
